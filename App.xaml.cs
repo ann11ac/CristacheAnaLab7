59d8cf3b93c8e36bb39d3e56e7a0a5182975a573
ann11ac <cristacheanacraita@gmail.com>
2023-11-20 13:44:47 +0200

    .

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace CristacheAnaLab7
+﻿using System;
+using CristacheAnaLab7.Data;
+using System.IO;
+
+namespace CristacheAnaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
